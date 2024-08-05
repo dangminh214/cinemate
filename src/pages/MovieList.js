4a0cd3d23cf17bf8c6d2f18347e71ce1a448e93d
dangminh214 <dangminh214@gmail.com>
2024-08-05 09:34:55 +0200

    upload project movie list

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,20 +2,33 @@ import { useFetch } from "../hooks/useFetch";
 import { useTitle } from "../hooks/useTitle";
 import { Card } from "../components";
 
-
-export const MovieList = ({apiPath, title}) => {
-  const { data: movies } = useFetch(apiPath);
+export const MovieList = ({ apiPath, title }) => {
+  const { data: movies, error, isLoading } = useFetch(apiPath);
   useTitle(title);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-        <div className="flex justify-start flex-wrap other:justify-evenly">       
-          { movies.map((movie) => (
-            <Card key={movie.id} movie={movie} />
-          )) }          
+        <div className="flex justify-start flex-wrap other:justify-evenly">
+          {Array.isArray(movies) && movies.length > 0 ? (
+            movies.map((movie) => <Card key={movie.id} movie={movie} />)
+          ) : (
+            <div>
+              <p className="text-slate-800 dark:text-slate-50">
+                No movies found.
+              </p>
+            </div>
+          )}
         </div>
       </section>
     </main>
-  )
-}
+  );
+};
